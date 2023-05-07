3af739e399dd4fb14a41fd9c61830aca5f4aa729
liyan-227 <lianpmu@webmail.umm.ac.id>
2023-05-07 14:07:46 +0700

    landing page step 1

--- weirditor/resources/views/welcome.blade.php
+++ weirditor/resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <title>Weirditor</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -19,14 +20,17 @@
             <div class="container px-5">
                 <img src="{{asset('img/logo.png')}}" alt="">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Portofolio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About Us</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item "><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item ms-5"><a class="nav-link" href="#!">Portofolio</a></li>
+                        <li class="nav-item ms-5"><a class="nav-link" href="#!">About Us</a></li>
+                        <li class="nav-item ms-5"><a class="nav-link" href="#!">Contact</a></li>
                     </ul>
                 </div>
+                <span class="material-symbols-outlined">
+                    search
+                    </span>
             </div>
         </nav>
         <hr class="container">
