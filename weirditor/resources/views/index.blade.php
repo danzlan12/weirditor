ad15c316d4dd09f7fd8ed5783e3d029d05b27090
liyan-227 <lianpmu@webmail.umm.ac.id>
2023-06-08 11:19:12 +0700

    3 section clear & responsive

--- weirditor/resources/views/index.blade.php
+++ weirditor/resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <meta name="author" content="" />
         <title>Weirditor</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="{{asset('img/logo.png')}}" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -43,8 +43,8 @@
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-6">
                         <div class="text-center my-5">
-                            <h1 class="display-5 fw-bolder text-black mb-2">Hello ©  مرحبًا</h1>
-                            <p class="lead text-black-50 mb-4">こんにちは ✱ Bonjour </p>
+                            <h1 class="display-5 fw-bolder text-black mb-2">Hello ©   <img src="{{asset('img/Line.png')}}" alt=""> مرحبًا</h1>
+                            <p class="lead text-black-50 mb-4"><h1>こんにちは <strong class="bintang">✱</strong> Bonjour</h1>  </p>
                          
                         </div>
                     </div>
@@ -52,19 +52,55 @@
             </div>
         </header>
       
-     {{-- slide 2 --}}
-     <header class="container py-5 mt-5"  style="border: 1px solid black;">
+     {{-- Content 2 --}}
+     <div class="container py-5 mt-5"  style="border: 1px solid black;">
                      <div class="row">
-                        <div class="teks col-6" style="padding: 100px">
-                            <h1><i>We are</i><strong> solution for your aesthetic ✱ 
+                        <div class="teks col-lg-6" style="padding: 100px">
+                            <h1><i>We are</i><strong> solution for your aesthetic <strong class="bintang">✱</strong>
                                 need to improve 
                                 your building quality</strong>  
                                 </h1>
                         </div>
-                        <div class="col-6 justify-content-center">
-                        123
+                        <div class="col-lg-6 justify-content-center">
+                        <div class="row justify-content-center">
+                            <a class="button col-3"><h4>Studio</h4></a>
+                            <a class="button col-4"><h4>Architecture</h4></a>
+                            <a class="button col-4"><h4>Aesthetic</h4></a>
+                        </div>
+                            <div class="col"><p class="tulisan mt-5">Welcome to our architecture studio's portfolio! We are a team of passionate architects and designers who are dedicated to bringing your vision to life. Our studio is committed to delivering exceptional designs that are not only aesthetically pleasing, but also functional and sustainable. We specialize in various architectural and interior design projects, including residential, commercial, and public spaces. With our expertise that every detail is carefully considered and executed.</p></div>
+                            <div>
+                                <a class="kotak" href="">Explore Me<a class="kotak1" href=""><img src="{{asset('img/panah.png')}}" alt=""></a></a>
+                            </div>
                      </div>
-    </header>
+                    </div>
+     </div>
+
+     {{-- Content 3 --}}
+
+     <div class="konten3 container">
+        <div class="col text-center"><h1> <strong>Showcase</strong> our
+           </h1>
+        <h1> <strong class="bintang">✱</strong> - best <u>work</u></h1>
+        </div>
+        <div class="text-center mt-5">
+            <a class="col ">SHOW ALL</a>
+            <a class="col ms-md-5">INTERIOR</a>
+            <a class="col ms-md-5">EKSTERIOR</a>
+            <a class="col ms-md-5">ARCHITECTURE</a>
+    </div>
+     </div>
+     
+<div class="container">
+    <div class="row justify-content-center">
+    <a class="card-bisnis card col-md-5 pt-5 mt-5 text-decoration-none text-black" href=""><img class="img-fluid ms-5 me-5" src="{{asset('img/example.png')}}" alt=""><h6><strong>Geometrical Pattern</strong>© Luciana Hotel</h6><img class="panah-card" src="{{asset('img/panah.png')}}" alt=""></a>
+    <a class="card-bisnis card col-md-5 pt-5 mt-5 ms-2 text-decoration-none text-black" href=""><img class="img-fluid ms-5 me-5" src="{{asset('img/example.png')}}" alt=""><h6><strong>Geometrical Pattern</strong>© Luciana Hotel</h6><img class="panah-card" src="{{asset('img/panah.png')}}" alt=""></a>
+    <a class="card-bisnis card col-md-5 pt-5 mt-4 ms-2 text-decoration-none text-black" href=""><img class="img-fluid ms-5 me-5" src="{{asset('img/example.png')}}" alt=""><h6><strong>Geometrical Pattern</strong>© Luciana Hotel</h6><img class="panah-card" src="{{asset('img/panah.png')}}" alt=""></a>
+    <a class="card-bisnis card col-md-5 pt-5 mt-4 ms-2 text-decoration-none text-black" href=""><img class="img-fluid ms-5 me-5" src="{{asset('img/example.png')}}" alt=""><h6><strong>Geometrical Pattern</strong>© Luciana Hotel</h6><img class="panah-card" src="{{asset('img/panah.png')}}" alt=""></a>
+    <a class="card-bisnis card col-md-5 pt-5 mt-4 ms-2 text-decoration-none text-black" href=""><img class="img-fluid ms-5 me-5" src="{{asset('img/example.png')}}" alt=""><h6><strong>Geometrical Pattern</strong>© Luciana Hotel</h6><img class="panah-card" src="{{asset('img/panah.png')}}" alt=""></a>
+    <a class="card-bisnis card col-md-5 pt-5 mt-4 ms-2 text-decoration-none text-black" href=""><img class="img-fluid ms-5 me-5" src="{{asset('img/example.png')}}" alt=""><h6><strong>Geometrical Pattern</strong>© Luciana Hotel</h6><img class="panah-card" src="{{asset('img/panah.png')}}" alt=""></a>
+</div>
+</div>
+
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
